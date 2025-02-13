08f9f10746671c32bf148c653631217d61f32c80
Meir Rotstein <mrotstein@fireblocks.com>
2025-02-14 00:45:10 +0200

    change log entry

--- api/onMessage.ts
+++ api/onMessage.ts
@@ -18,7 +18,11 @@ module.exports = async (req, res) => {
   if (req.method !== "POST")
     return res.status(405).json({ error: "Method not allowed" });
 
-  console.log(req.body);
+  try {
+    console.log(JSON.stringify(req.body));
+  } catch (e) {
+    console.log(req.body);
+  }
 
   res.json({ status: "Message received" });
 };
