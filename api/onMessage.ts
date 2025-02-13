e1dad6d14e0b7b11f1f2f05b82266c6a47e03e00
Meir Rotstein <mrotstein@fireblocks.com>
2025-02-13 23:38:07 +0200

    add GET for verification call

--- api/onMessage.ts
+++ api/onMessage.ts
@@ -1,7 +1,24 @@
+const verifyToken = process.env.VERIFY_TOKEN;
+
 module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: "Method not allowed" });
+  if (req.method === "GET") {
+    // verification request
+    console.log("verification request", req.query);
+    if (
+      req.query["hub.mode"] !== "subscribe" ||
+      req.query["hub.verify_token"] !== process.env.VERIFY_TOKEN
+    ) {
+      return res.status(403).send("Validation Error");
+    }
+    return req.query["hub.challenge"]
+      ? res.status(200).send(req.query["hub.challenge"])
+      : res.status(400).send("Error");
+  }
+
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Method not allowed" });
 
   console.log(req.body);
-  
+
   res.json({ status: "Message received" });
 };
