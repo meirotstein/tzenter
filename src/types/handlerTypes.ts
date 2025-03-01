270b42e8150f77768b56881beb8ec4a72cc1e861
Meir Rotstein <mrotstein@fireblocks.com>
2025-03-02 00:57:42 +0200

    add whatsapp types

---
+++ src/types/handlerTypes.ts
@@ -0,0 +1,14 @@
+export enum Endpoint {
+  ON_MESSAGE = "onMessage",
+}
+
+export type HandlerRequest = {
+  query?: Record<string, string | string[]>;
+  body?: Record<string, any>;
+};
+
+export type HandlerResponse = string | Record<string, any> | void;
+
+export interface IHandler {
+  handle(req: HandlerRequest): Promise<HandlerResponse>;
+}
---
+++ src/types/whatsapp/types/README.md
@@ -0,0 +1,2 @@
+types copied from node_modules/whatsapp/src/types
+v.0.0.5-Alpha
