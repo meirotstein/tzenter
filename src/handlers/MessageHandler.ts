ef18e723f7300b9279b53d0547cae949b7c27c4b
Meir Rotstein <mrotstein@fireblocks.com>
2025-03-02 10:22:40 +0200

    use senderId env configuration

--- src/handlers/MessageHandler.ts
+++ src/handlers/MessageHandler.ts
@@ -3,12 +3,13 @@ import { WebhookObject } from "../types/whatsapp/types/webhooks";
 import { WhatsappClient } from '../clients/WhatsappClient';
 
 export class MessageHandler implements IHandler {
+  
   async handle(req: HandlerRequest): Promise<HandlerResponse> {
     console.log("incoming message request", req.query);
 
     try {
       const message = req.body as WebhookObject;
-      const waClient = new WhatsappClient(581926268335637);
+      const waClient = new WhatsappClient(Number(process.env.WA_PHONE_NUMBER_ID));
       const recipientPhoneNum = message.entry[0].changes[0].value.contacts[0].wa_id;
       const resp = await waClient.sendTextMessage(+recipientPhoneNum, `התקבל ${new Date().toLocaleString()}`);
       console.log("response from whatsapp", resp);
