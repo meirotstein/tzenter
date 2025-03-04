196a61977cae18798e804c0a1df24e80cee030d6
Meir Rotstein <mrotstein@fireblocks.com>
2025-03-04 22:19:38 +0200

    test usage for KV

--- src/handlers/MessageHandler.ts
+++ src/handlers/MessageHandler.ts
@@ -1,10 +1,10 @@
 import { HandlerRequest, HandlerResponse, IHandler } from "./types";
 import { WebhookObject } from "../external/whatsapp/types/webhooks";
-import { WhatsappClient } from '../clients/WhatsappClient';
+import { WhatsappClient } from "../clients/WhatsappClient";
 import { extractTextMessage } from "../utils";
+import { KVClient } from "../clients/KVClient";
 
 export class MessageHandler implements IHandler {
-  
   async handle(req: HandlerRequest): Promise<HandlerResponse> {
     console.log("incoming message request", JSON.stringify(req.body));
 
@@ -15,9 +15,27 @@ export class MessageHandler implements IHandler {
         // non-text message (video, status, etc)- currently not interested in handling
         return;
       }
-      const waClient = new WhatsappClient(Number(process.env.WA_PHONE_NUMBER_ID));
+
+      const kvClient = new KVClient(
+        process.env.KV_REST_API_TOKEN!,
+        process.env.KV_REST_API_URL!
+      );
+      await kvClient.set(message.recipient.phoneNum, {
+        name: message.recipient.name,
+      });
+
+      const nameFromKv = await kvClient.get(message.recipient.phoneNum);
+      console.log("name from kv", nameFromKv);
+
+      const waClient = new WhatsappClient(
+        Number(process.env.WA_PHONE_NUMBER_ID)
+      );
       const recipientPhoneNum = message.recipient.phoneNum;
-      const resp = await waClient.sendTextMessage(+recipientPhoneNum, `התקבל ${new Date().toLocaleString()}`);
+      const resp = await waClient.sendTextMessage(
+        +recipientPhoneNum,
+        `התקבל ${new Date().toLocaleString()}
+        מאת: ${nameFromKv?.name}`
+      );
       console.log("response from whatsapp", resp);
     } catch (e) {
       console.log(req.body);
