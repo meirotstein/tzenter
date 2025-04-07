6d67e7fde1516fb00fdeb65faa586e18a72b680a
Meir Rotstein <mrotstein@fireblocks.com>
2025-04-07 11:33:46 +0300

    remove cron-test endpoint

--- src/api/cron-test.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  console.log("cron test started");
-  return res.status(200).send({ status: "ok" });
-};
