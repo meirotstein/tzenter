5d14860140914b48bdae81f7ec500a7c31f97f72
Meir Rotstein <mrotstein@fireblocks.com>
2025-04-10 16:38:03 +0300

    Revert "use tsup as build bundler"
    This reverts commit 279a088ff4721b7e3b3c2a84f8af5270aea91a6a.

--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts", "src/api/onSchedule.ts", "src/api/onMessage.ts"],
-  format: ["esm"],
-  dts: true,
-  outDir: "dist",
-});
