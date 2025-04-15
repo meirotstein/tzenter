af6ae14ddaea38318cf933c9f5756843d62d485d
Meir Rotstein <mrotstein@fireblocks.com>
2025-04-15 15:28:08 +0300

    add another endpoint for experimental packages

---
+++ src/api/onDate.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { getHolidaysOnDate } from "@hebcal/core";
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+
+
+  try {
+    return res.status(200).send(getHolidaysOnDate(new Date()););
+  } catch (e: any) {
+    console.error("scheduled invocation failed", e);
+    return res.status(500).send(e.message);
+  }
+};
