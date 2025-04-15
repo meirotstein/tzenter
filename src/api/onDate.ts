abd7b134949fc2fc433adbdfe10a3bba487b5ae9
Meir Rotstein <mrotstein@fireblocks.com>
2025-04-15 18:08:21 +0300

    experimental: try import hebcal dynamically

--- src/api/onDate.ts
+++ src/api/onDate.ts
@@ -1,11 +1,14 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { getHolidaysOnDate } from "@hebcal/core";
+// import { getHolidaysOnDate } from "@hebcal/core";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-
+  async function getHebrewHolidays(date: Date, il = true) {
+    const { getHolidaysOnDate } = await import("@hebcal/core");
+    return getHolidaysOnDate(date, il);
+  }
 
   try {
-    return res.status(200).send('getHolidaysOnDate(new Date())');
+    return res.status(200).send(getHebrewHolidays(new Date()));
   } catch (e: any) {
     console.error("scheduled invocation failed", e);
     return res.status(500).send(e.message);
