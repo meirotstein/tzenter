791720333aad9f3ec6059433773506cda6e0e116
Meir Rotstein <mrotstein@fireblocks.com>
2025-04-15 15:31:19 +0300

    experimental: return static answer

--- src/api/onDate.ts
+++ src/api/onDate.ts
@@ -5,7 +5,7 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
 
 
   try {
-    return res.status(200).send(getHolidaysOnDate(new Date()););
+    return res.status(200).send('getHolidaysOnDate(new Date())');
   } catch (e: any) {
     console.error("scheduled invocation failed", e);
     return res.status(500).send(e.message);
